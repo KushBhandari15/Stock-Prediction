4dd8af604c8efad78b52cceb81902f1222e26d21
Kush Bhandari <98527317+KushBhandari15@users.noreply.github.com>
2024-03-21 12:46:23 -0400

    Create unit_test.py
    Created a new directory for tests and moved the unit test file inside the new directory

---
+++ tests/unit_test.py
@@ -0,0 +1,32 @@
+import unittest
+from main import test_stock_prediction
+
+class TestStockPredictionApp(unittest.TestCase):
+    
+    def test_select_invalid_stock(self):
+        result = test_stock_prediction('XYZ', "2010-01-01", 2*365)
+        expected_output = False
+        self.assertEqual(result, expected_output)
+            
+    def test_select_stock_and_forecast_duration(self):
+        result = test_stock_prediction('AAPL', "2010-01-01", 2*365)
+        expected_output = True
+        self.assertEqual(result, expected_output)    
+        
+    def test_case_sensitive_input(self):
+        result = test_stock_prediction('aapl', "2015-01-01", 365)
+        expected_output = True
+        self.assertEqual(result, expected_output)
+        
+    def test_select_stock_and_forecast_duration2(self):
+        result = test_stock_prediction('MSFT', "2015-01-01", 1*365)
+        expected_output = True
+        self.assertEqual(result, expected_output)
+        
+    def test_empty_input(self):
+        result = test_stock_prediction('', "2015-01-01", 365)
+        expected_output = False
+        self.assertEqual(result, expected_output)
+
+if __name__ == '__main__':
+    unittest.main()
