fc28e5fe0bae3f1913cb1b8d3717e4dd23721297
Kush Bhandari <98527317+KushBhandari15@users.noreply.github.com>
2024-04-23 02:42:52 -0400

    Update unit_test.py

--- tests/unit_test.py
+++ tests/unit_test.py
@@ -1,32 +1,51 @@
 import unittest
-from testing_stock_prediction.py import test_stock_prediction
+from testing_stock_prediction import test_stock_prediction
 
 class TestStockPredictionApp(unittest.TestCase):
+    """
+    A test case class for the stock prediction application.
+    """
     
     def test_select_invalid_stock(self):
-        result = test_stock_prediction('XYZ', "2010-01-01", 2*365)
+        """
+        Test case for selecting an invalid stock.
+        """
+        result = test_stock_prediction('XYZ', "2010-01-01", 2 * 365)
         expected_output = False
         self.assertEqual(result, expected_output)
             
     def test_select_stock_and_forecast_duration(self):
-        result = test_stock_prediction('AAPL', "2010-01-01", 2*365)
+        """
+        Test case for selecting a valid stock and forecast duration.
+        """
+        result = test_stock_prediction('AAPL', "2010-01-01", 2 * 365)
         expected_output = True
         self.assertEqual(result, expected_output)    
         
     def test_case_sensitive_input(self):
+        """
+        Test case for case-sensitive input.
+        """
         result = test_stock_prediction('aapl', "2015-01-01", 365)
         expected_output = True
         self.assertEqual(result, expected_output)
         
     def test_select_stock_and_forecast_duration2(self):
-        result = test_stock_prediction('MSFT', "2015-01-01", 1*365)
+        """
+        Test case for selecting a stock and a different forecast duration.
+        """
+        result = test_stock_prediction('MSFT', "2015-01-01", 1 * 365)
         expected_output = True
         self.assertEqual(result, expected_output)
         
     def test_empty_input(self):
+        """
+        Test case for empty input.
+        """
         result = test_stock_prediction('', "2015-01-01", 365)
         expected_output = False
         self.assertEqual(result, expected_output)
 
 if __name__ == '__main__':
     unittest.main()
+
